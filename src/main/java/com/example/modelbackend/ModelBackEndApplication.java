27ba2edd87947244ea6ad9228c3d3486b51bd00c
Serban Gherescu <sgherescu@yahoo.com>
2023-09-04 18:11:15 +0300

    initial commit

---
+++ src/main/java/com/example/modelbackend/ModelBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.modelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelBackEndApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/modelbackend/entity/Product.java
@@ -0,0 +1,2 @@
+package com.example.modelbackend.entity;public class Product {
+}
