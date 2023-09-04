75ae63d3762df70fb9f6e0fce36ffd06cc845f93
Serban Gherescu <sgherescu@yahoo.com>
2023-09-04 21:07:52 +0300

    created resources for jpa interfaces

--- src/main/java/com/example/modelbackend/data_access_object/ProductCategoryRepository.java
+++ src/main/java/com/example/modelbackend/data_access_object/ProductCategoryRepository.java
@@ -1,5 +1,12 @@
 package com.example.modelbackend.data_access_object;
 
-public interface ProductCategoryRepository {
+import com.example.modelbackend.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "products_repository", path = "products_repository")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+
 
 }
